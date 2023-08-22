9978603974beed80ec5afbb7c1e3a687e69166aa
zoccalucas <lucaszoccasoares@gmail.com>
2023-08-22 16:20:45 -0300

    feat: validates endpoint operation

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,22 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Footer from "./Components/Footer";
+import Header from "./Components/Header";
+import Home from "./Components/Home";
+import Api from './api/Api';
+
 const App = () => {
-  return <div>App React</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
---
+++ src/Components/Home.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import Api from '../api/Api';
+
+const Home = () => {
+  return (<div><Api /></div>)
+}
+
+export default Home
