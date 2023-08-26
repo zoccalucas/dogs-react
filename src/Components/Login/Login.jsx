dac13e2bb28c0ef70fea7538af3beb7fafb4c73b
zoccalucas <lucaszoccasoares@gmail.com>
2023-08-26 16:46:56 -0300

    feat: adds login routes

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import LoginForm from '../Login/LoginForm';
+import LoginCreate from '../Login/LoginCreate';
+import LoginPasswordLost from '../Login/LoginPasswordLost';
+import LoginPasswordReset from '../Login/LoginPasswordReset';
+import { Routes, Route } from 'react-router-dom';
 
 const Login = () => {
   return (
     <div>
-      Login
+      <Routes>
+        <Route path="/" element={<LoginForm />}/>
+        <Route path="create" element={<LoginCreate />}/>
+        <Route path="lost" element={<LoginPasswordLost />}/>
+        <Route path="reset" element={<LoginPasswordReset />}/>
+      </Routes>
     </div>
   )
 };
---
+++ src/Components/Login/LoginCreate.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function LoginCreate() {
+  return (
+    <div>
+      Login Create
+    </div>
+  )
+}
+
+export default LoginCreate;
