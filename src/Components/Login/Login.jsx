fb9a8fcc05eef0a8de2f8dbdd83d5ce94da46bc2
Lucas Zocca <lucaszoccasoares@gmail.com>
2023-09-09 15:43:58 -0300

    feat: add login background

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -5,20 +5,23 @@ import LoginPasswordLost from '../Login/LoginPasswordLost';
 import LoginPasswordReset from '../Login/LoginPasswordReset';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import { UserContext } from '../../UserContext';
+import styles from './Login.module.css';
 
 const Login = () => {
   const { login } = React.useContext(UserContext);
 
   if(login === true) return <Navigate to="/conta" />;
   return (
-    <div>
-      <Routes>
-        <Route path="/" element={<LoginForm />}/>
-        <Route path="create" element={<LoginCreate />}/>
-        <Route path="lost" element={<LoginPasswordLost />}/>
-        <Route path="reset" element={<LoginPasswordReset />}/>
-      </Routes>
-    </div>
+    <section className={styles.login}>
+      <div className={styles.forms}>
+        <Routes>
+          <Route path="/" element={<LoginForm />}/>
+          <Route path="create" element={<LoginCreate />}/>
+          <Route path="lost" element={<LoginPasswordLost />}/>
+          <Route path="reset" element={<LoginPasswordReset />}/>
+        </Routes>
+      </div>
+    </section>
   )
 };
 
